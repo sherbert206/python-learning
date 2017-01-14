626609d426acd2786d0dd1bada02c9f90ebcd68d
sherbert206 <sherbert206@gmail.com>
2017-01-13 16:54:29 -0800

    Add files via upload

---
+++ variables and types scripts.py
@@ -0,0 +1,18 @@
+myint = 69
+print(myint);  # integer variables are just normal numbers
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat);  # floating variables are normal integers just with decimal values
+
+mystring = 'hello'
+print (mystring)
+mystring2 = "hello"
+print (mystring2);  # strings are just words and sentences. does not need quotations just a '
+# you can add strings together by doing string1+string2= new string
+
+a, b = 3, 4
+print(a, b) #this adds a parenthesis around the combination of numbers
+#but you can print 2 values on the same line
+
